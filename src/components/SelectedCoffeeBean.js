e9ac3dc3b43ca0dfa1ce4ee6241143c8dff661ed
Luis <manriqueleo98@gmail.com>
2023-07-06 17:15:11 -0500

    practice solved

--- src/components/SelectedCoffeeBean.js
+++ src/components/SelectedCoffeeBean.js
@@ -1,7 +1,11 @@
+import { useContext } from "react";
+import { CoffeeContext } from "../context/CoffeeContext";
+
 const SelectedCoffeeBean = () => {
+  const {coffeeBean} = useContext(CoffeeContext)
   return (
     <div className="selected-coffee">
-      <h2>Current Selection: </h2>
+      <h2>Current Selection: {coffeeBean.name}</h2>
     </div>
   );
 }
